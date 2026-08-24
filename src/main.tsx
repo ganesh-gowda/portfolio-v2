777aae44ce6101d2cd9e4a1aadc02c584473d5ab
Ganesh Gowda M <ganeshgowdam6@gmail.com>
2026-08-24 16:57:10 +0530

    Apply new typography system

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './landing.css';
 import './about.css';
+import './typography.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
